1bdff887fd8170b21bfef6917fe98c9832055636
Gustavo <gustavomacedosousa013@gmail.com>
2023-06-27 20:06:11 -0300

    Segunda etapa -adicionando um input para receber o nome do usuário e adicionando a função com rebase

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import datetime
+
+ano_atual = datetime.datetime.now().year
+def funcao():
+    nome = input("Qual o seu nome?").title()
+    try:
+        idade = int(input("Idade:"))
+    except ValueError:
+        print("Só números inteiros!")
+        idade = int(input("Idade:"))
+    print(f"Olá {nome.split()[0]}!")
+    print(f"Você nasceu em {ano_atual - idade}")
+    print("Prazer em conhecê-lo")
+
+
 if __name__ == "__main__":
-    print("Olá Mundo!")
+    funcao()
